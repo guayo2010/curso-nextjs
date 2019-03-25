daf45c7c9a42e932d3d5abdaf0f737e6b980af6d
Darwin Morocho <dsmr.apps@gmail.com>
2019-03-24 20:02:07 -0500

    clase 03

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,34 @@
 import Head from 'next/head';
 import Link from 'next/link';
+import styled from 'styled-components';
+import './styles.css';
+
+const Banner = styled.div`
+  position: relative;
+  width: 100%;
+  height: 70vh;
+  background-image: url('/static/bg/banner.jpg');
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+
+  .movie-description {
+    position: absolute;
+    bottom: 0;
+    left: 0;
+    width: 100%;
+    min-height: 200px;
+    background-image: linear-gradient(
+      to top,
+      rgba(0, 0, 0, 1),
+      rgba(0, 0, 0, 0)
+    );
+    h2 {
+      color: #fff;
+      text-transform: uppercase;
+    }
+  }
+`;
 
 const Index = () => (
   <div>
@@ -7,9 +36,17 @@ const Index = () => (
       <title>INdex</title>
     </Head>
 
-    <Link href={{ pathname: 'contact', query: { id: 13, lang: 'es' } }}>
-      <a>Ir a contactos</a>
-    </Link>
+    <Banner>
+      <div className="movie-description container">
+        <h2>Furia de titanes</h2>
+        <div className="d-flex">
+          <a className="btn-primary">Ver Pelicula</a>
+          <a className="btn-border" style={{ marginLeft: 10 }}>
+            Ver Info
+          </a>
+        </div>
+      </div>
+    </Banner>
   </div>
 );
 export default Index;
